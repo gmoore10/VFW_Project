533345fb62f8b32049a7de645ddda6826d836ae5
Garrett Moore <gmoore10@fullsail.edu>
2013-01-30 21:31:31 -0500

    Added JSON pre-populated data function

---
+++ js/json.js
@@ -0,0 +1,32 @@
+﻿var json = {
+    "To-Do 1": {
+        "firstName": ["First Name:", "Garrett"],
+        "lastName": ["Last Name:", "Moore"],
+        "toDoName": ["To-Do Name:", "Unit Test To-Do 1"],
+        "dtDue": ["Due Date:", "7/31/2013"],
+        "assignedTo": ["Assigned To:", "Jim"],
+        "priority": ["Priority:", "27"],
+        "sendEmail": ["Send Email to Task Receiver:", "Yes"],
+        "content": ["Content: ", "This is a text placeholder for content for our Unit Test To-Do 1."],
+    },
+    "To-Do 2": {
+        "firstName": ["First Name:", "Jamie"],
+        "lastName": ["Last Name:", "Moore"],
+        "toDoName": ["To-Do Name:", "Unit Test To-Do 2"],
+        "dtDue": ["Due Date:", "9/14/2013"],
+        "assignedTo": ["Assigned To:", "Jim"],
+        "priority": ["Priority:", "33"],
+        "sendEmail": ["Send Email to Task Receiver:", "No"],
+        "content": ["Content: ", "This is a text placeholder for content for our Unit Test To-Do 2."],
+    },
+    "To-Do 3": {
+        "firstName": ["First Name:", "Wade"],
+        "lastName": ["Last Name:", "Moore"],
+        "toDoName": ["To-Do Name:", "Unit Test To-Do 3"],
+        "dtDue": ["Due Date:", "12/2/2013"],
+        "assignedTo": ["Assigned To:", "Kim"],
+        "priority": ["Priority:", "37"],
+        "sendEmail": ["Send Email to Task Receiver:", "Yes"],
+        "content": ["Content: ", "This is a text placeholder for content for our Unit Test To-Do 3."],
+    }
+}
